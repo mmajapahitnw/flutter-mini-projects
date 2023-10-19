9c4adcacab45abff9f1b6a7beccb77a181afe5da
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-19 14:56:05 +0700

    Create board.dart

---
+++ tetris/lib/board.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GameBoard extends StatefulWidget {
+  const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  int rowLength = 10;
+  int colLength = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: rowLength),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Container(
+                child: Center(
+                    child: Text(index.toString(), style: TextStyle(color: Colors.white),)),
+              ),
+            );
+          }),
+    );
+  }
+}
