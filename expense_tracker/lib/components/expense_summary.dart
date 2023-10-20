fbde6386fdbafb5749f01b4674b1f415e6fd0e8d
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-20 17:55:38 +0700

    Update expense_summary.dart

--- expense_tracker/lib/components/expense_summary.dart
+++ expense_tracker/lib/components/expense_summary.dart
@@ -1,10 +1,32 @@
+import 'package:expense_tracker/bar_graph/bar_graph.dart';
+import 'package:expense_tracker/data/expense_data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ExpenseSummary extends StatelessWidget {
-  const ExpenseSummary({super.key});
+  final DateTime startOfWeek;
+
+  const ExpenseSummary({
+    super.key,
+    required this.startOfWeek,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ExpenseData>(
+      builder: (context, value, child) => SizedBox(
+        height: 200,
+        child: MyBarGraph(
+          maxY: 100,
+          monAmount: 5,
+          tueAmount: 43,
+          wedAmount: 56,
+          thuAmount: 23,
+          friAmount: 65,
+          satAmount: 90,
+          sunAmount: 99,
+        ),
+      ),
+    );
   }
 }
