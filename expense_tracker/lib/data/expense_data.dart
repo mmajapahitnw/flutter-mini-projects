d8e65196de8a17d9137f5503fcdb9bd2e438ab6b
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-20 07:24:26 +0700

    Update expense_data.dart

--- expense_tracker/lib/data/expense_data.dart
+++ expense_tracker/lib/data/expense_data.dart
@@ -1,16 +1,62 @@
-import '';
+
+
+import 'package:expense_tracker/models/expense_item.dart';
 
 class ExpenseData {
   // list of all expenses
-  ExpenseItem overallExpense = [];
+  List<ExpenseItem> overallExpenseList = [];
 
   // get expense list
+  List<ExpenseItem> getOverallExpenseList() {
+    return overallExpenseList;
+  }
 
   // add new expense
+  void addNewExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+  }
 
   // delete expense
+  void deleteExpense(ExpenseItem expense) {
+    overallExpenseList.remove(expense);
+  }
 
   // get weekday (sun, mon, tue, etc) from DateTime object
+  String getDayName (DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thu';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+    }
+  }
 
   // get the first weekday (mon)
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+
+    //get today's date
+    DateTime today = DateTime.now();
+
+    // roll back until we find monday
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Mon') {
+        startOfWeek = today.subtract(Duration(days: i));
+      }
+    }
+
+    return startOfWeek!;
+  }
 }
