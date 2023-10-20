e6482054c54283c709902b407b3787a91ebbb6f5
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-20 10:15:20 +0700

    Update home_page.dart

--- expense_tracker/lib/pages/home_page.dart
+++ expense_tracker/lib/pages/home_page.dart
@@ -8,10 +8,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // add new expense
+  void addNewExpense() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add new expense'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.grey[300],
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewExpense,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
