7123499445b6433b0abac627980014afc4958359
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-20 17:55:36 +0700

    Create bar_graph.dart

---
+++ expense_tracker/lib/bar_graph/bar_graph.dart
@@ -0,0 +1,57 @@
+import 'package:expense_tracker/bar_graph/bar_data.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class MyBarGraph extends StatelessWidget {
+  final double? maxY;
+
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thuAmount;
+  final double friAmount;
+  final double satAmount;
+  final double sunAmount;
+
+  const MyBarGraph({
+    super.key,
+    required this.maxY,
+    required this.monAmount,
+    required this.tueAmount,
+    required this.wedAmount,
+    required this.thuAmount,
+    required this.friAmount,
+    required this.satAmount,
+    required this.sunAmount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // initialize the bar graph
+    BarData myBarData = BarData(
+      monAmount: monAmount,
+      tueAmount: tueAmount,
+      wedAmount: wedAmount,
+      thuAmount: thuAmount,
+      friAmount: friAmount,
+      satAmount: satAmount,
+      sunAmount: sunAmount,
+    );
+    myBarData.initializeBarData();
+
+    return BarChart(
+      BarChartData(
+        maxY: maxY,
+        minY: 0,
+        barGroups: myBarData.barData.map(
+          (data) => BarChartGroupData(
+            x: data.x,
+            barRods: [
+              BarChartRodData(toY: data.y),
+            ],
+          ),
+        ).toList()
+      ),
+    );
+  }
+}
