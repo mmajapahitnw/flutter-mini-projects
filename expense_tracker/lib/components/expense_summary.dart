778d21b9d49cfd80659b21012bec4ce24c407bba
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-20 18:49:07 +0700

    Update expense_summary.dart

--- expense_tracker/lib/components/expense_summary.dart
+++ expense_tracker/lib/components/expense_summary.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/bar_graph/bar_graph.dart';
 import 'package:expense_tracker/data/expense_data.dart';
+import 'package:expense_tracker/date_time/date_time_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,6 +14,15 @@ class ExpenseSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // get yyyymmdd for each day in this week
+    String monday = convertDateTimeToString(startOfWeek.add(Duration(days: 0)));
+    String tuesday = convertDateTimeToString(startOfWeek.add(Duration(days: 1)));
+    String wednesday = convertDateTimeToString(startOfWeek.add(Duration(days: 2)));
+    String thursday = convertDateTimeToString(startOfWeek.add(Duration(days: 3)));
+    String friday = convertDateTimeToString(startOfWeek.add(Duration(days: 4)));
+    String saturday = convertDateTimeToString(startOfWeek.add(Duration(days: 5)));
+    String sunday = convertDateTimeToString(startOfWeek.add(Duration(days: 6)));
+
     return Consumer<ExpenseData>(
       builder: (context, value, child) => SizedBox(
         height: 200,
