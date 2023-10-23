dccfc573536f6fe21753f3831a363154800f0db5
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-10-23 14:07:12 +0700

    Create article util

---
+++ dicoding_news_app/lib/util/articles.dart
@@ -0,0 +1,19 @@
+class Article {
+  final String author;
+  final String title;
+  final String desc;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  Article ({
+    required this.author,
+    required this.title,
+    required this.desc,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+}
