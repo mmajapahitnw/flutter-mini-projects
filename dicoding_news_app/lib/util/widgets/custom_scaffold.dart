b0a10f641e77b8f3f7f69998f624203928ef48df
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-11-05 14:59:35 +0700

    Complete

---
+++ dicoding_news_app/lib/util/widgets/custom_scaffold.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomScaffold extends StatelessWidget {
+  final Widget body;
+
+  const CustomScaffold({
+    super.key,
+    required this.body,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            body,
+            Card(
+              shape: const BeveledRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(16),
+                ),
+              ),
+              margin: const EdgeInsets.all(0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: Text(
+                      'N',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
