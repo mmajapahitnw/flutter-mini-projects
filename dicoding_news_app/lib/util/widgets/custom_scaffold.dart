24ff8e5089ab2e548905d616b7532d9385e298cd
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-11-05 20:18:12 +0700

    Update custom_scaffold.dart

--- dicoding_news_app/lib/util/widgets/custom_scaffold.dart
+++ dicoding_news_app/lib/util/widgets/custom_scaffold.dart
@@ -15,34 +15,45 @@ class CustomScaffold extends StatelessWidget {
         child: Stack(
           children: [
             body,
-            Card(
-              shape: const BeveledRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(16),
-                ),
-              ),
-              margin: const EdgeInsets.all(0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(Icons.arrow_back),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: Text(
-                      'N',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  )
-                ],
-              ),
-            )
+            buildShortAppBar()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class buildShortAppBar extends StatelessWidget {
+  const buildShortAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: const BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(16),
+        ),
+      ),
+      margin: const EdgeInsets.all(0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text(
+              'N',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          )
+        ],
       ),
     );
   }
